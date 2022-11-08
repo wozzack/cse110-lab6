2eef8e7c6079cbcbdb17622610950d197710f85b
Alex Wu <alwu@ucsd.edu>
2022-11-07 18:24:27 -0800

    Upload A9-A11

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,11 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  let r = localStorage.getItem('recipes');
+  if (r == null || r.length == 0) {
+    return [];
+  }
+  else return JSON.parse(localStorage.getItem('recipes'));
 }
 
 /**
@@ -35,10 +40,16 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  let refToMain = document.querySelector("main");
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  for (let i = 0; i < recipes.length; i++) {
+    let recipeCard = document.createElement("recipe-card");
+    recipeCard.data = recipes[i];
+    refToMain.append(recipeCard);
+  }
 }
 
 /**
